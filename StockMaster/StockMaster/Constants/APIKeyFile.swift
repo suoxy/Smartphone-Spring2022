5f1da58b6a41169f726bfa21c4a2d8970fa5ee1d
suoxy <suo.x@northeastern.edu>
2022-02-27 17:05:40 -0800

    add code for stock master v1

---
+++ StockMaster/StockMaster/Constants/APIKeyFile.swift
@@ -0,0 +1,10 @@
+//
+//  APIKeyFile.swift
+//  StockMaster
+//
+//  Created by Xiyue Suo on 2/27/22.
+//
+
+import Foundation
+
+let apiKey = "fb50eac0cf5e33e571aa3275de628e2c"
---
+++ StockMaster/StockMaster/Model/StockShort.swift
@@ -0,0 +1,19 @@
+//
+//  StockShort.swift
+//  StockMaster
+//
+//  Created by Xiyue Suo on 2/27/22.
+//
+
+import Foundation
+import RealmSwift
+
+class StockShort : Object {
+    @objc dynamic var symbol : String = ""
+    @objc dynamic var price : Float = 0.0
+    @objc dynamic var volume : Int = 0
+    
+    override static func primaryKey() -> String? {
+        return "symbol"
+    }
+}
