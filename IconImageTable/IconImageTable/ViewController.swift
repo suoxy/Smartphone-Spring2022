b4f6bd67ccdf3a73be86a00517b805f036e7dc4c
suoxy <71334670+suoxy@users.noreply.github.com>
2022-03-26 19:37:34 -0700

    push icon without png

---
+++ IconImageTable/IconImageTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IconImageTable
+//
+//  Created by Xiyue Suo on 3/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
